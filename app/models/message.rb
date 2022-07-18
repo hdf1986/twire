244c3e245e5738a4065741e153a9d212bbbcc0c1
Hugo David Farji <hi@hugo.farji.me>
2022-07-18 16:31:53 -0300

    Added live update of feed

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,13 @@ class Message < ApplicationRecord
   belongs_to :user, optional: false
 
   validates :content, presence: true
+
+  after_create_commit :broadcast_message
+
+  private
+
+  def broadcast_message
+    broadcast_append_to user
+    user.followers.find_each { |u| broadcast_append_to u }
+  end
 end
