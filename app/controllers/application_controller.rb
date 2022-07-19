881d3ba7802fc681c286f7d399165c5861f257a3
Hugo David Farji <hi@hugo.farji.me>
2022-07-19 14:21:51 -0300

    Added related friends

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # See https://stackoverflow.com/a/42572759/4870465
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
--- app/controllers/signed_in_application_controller.rb
+++ app/controllers/signed_in_application_controller.rb
@@ -2,4 +2,11 @@
 
 class SignedInApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :fetch_recommended_users
+
+  private
+
+  def fetch_recommended_users
+    @recommended_users = User.all.where.not(id: current_user.followed.ids + [current_user.id])
+  end
 end
