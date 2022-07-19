6d698954f8b77a2ffd56d02282e23b383e99c59a
Hugo David Farji <hi@hugo.farji.me>
2022-07-19 13:35:10 -0300

    Updated styles for session and registration pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   # Intermediate tables
   has_many :follows
