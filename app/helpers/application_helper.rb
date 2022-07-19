6dc8069f6a64446f9a9ce13aa2540f99c1380ec4
Hugo David Farji <hi@hugo.farji.me>
2022-07-19 09:23:56 -0300

    Added tailwind & app styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def nav_link_to(text, path)
+    link_to text, path, class: 'group grid gap-x-2 gap-y-1 grid-cols-1 lg:grid-cols-auto-span items-center justify-items-center lg:justify-items-start mx-1 px-3 py-2 rounded-lg bg-transparent hover:bg-[#25263D] focus:bg-[#25263D] whitespace-nowrap'
+  end
 end
